6fcf202068d87036538a4485953e1123b7782a36
mazuh <marcell-mz@hotmail.com>
2023-02-06 13:01:52 -0300

    make it work

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# nhentai -- light scrapper
+# nhentai -- scrapper
 
-> A lightweight tool for scrapping 'nhentai' pages. -- エロ漫画 !
+> A simple tool for scrapping 'nhentai' pages. -- エロ漫画 !
+
+## How to use
+
+### Setting up
+
+Have Node 18 or higher installed and in use. Then prepare the dependencies:
+
+```
+npm install
+```
+
+Now find a gallery you'll want to scrap (it only works with `nhentai.net`
+and not its variations).
+
+### Running
+
+Assuming you have a gallery identifier, run for example:
+
+```
+npm start 363046
+```
+
+It'll print the images URLs in your terminal. And you can also download them:
+
+```
+npm start 363046 downloads/
+```
+
+A subdirectory called "downloads" will be created inside this repo folder
+(git-ignored if you name it like this) to store the scrapped images. You could
+use an absolute path to any other directory you want too.
--- src/main.js
+++ src/main.js
@@ -1,1 +1,190 @@
-console.log('Hello world.')
+"use strict";
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+const { executablePath } = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
+
+console.log("Gathering parameters...");
+
+// prepare id
+
+const firstArg = (process.argv[2] || "").trim();
+const galleryId = parseInt(firstArg, 10) ? firstArg : "";
+if (!galleryId) {
+  console.error(
+    "First argument is missing. It must be a nhentai.net gallery identifier (they're a small numerical sequence)."
+  );
+  process.exit(1);
+}
+
+console.log("Gallery identifier:", galleryId);
+
+// prepare urls
+
+const galleryUrl = new URL(`g/${galleryId}/`, "https://nhentai.net/");
+
+console.log("Gallery URL:", galleryUrl.toString());
+
+// prepare storage
+
+const secondArg = (process.argv[3] || "").trim();
+
+const storageRootPath =
+  !secondArg || secondArg.startsWith("/")
+    ? secondArg
+    : path.join("./", secondArg);
+
+if (storageRootPath) {
+  console.log("Downloads folder:", storageRootPath);
+} else {
+  console.log("Not storing images.");
+}
+
+// general config
+
+const isHeadless =
+  !!process.env.SCRAP_HEADLESS && process.env.SCRAP_HEADLESS !== "false";
+
+console.log("Display browser:", isHeadless ? "no" : "yes");
+
+// scrap!
+console.log();
+
+(async () => {
+  console.log("Launching navigation...");
+
+  const browser = await puppeteer.launch({
+    headless: isHeadless,
+    executablePath: executablePath(),
+  });
+
+  try {
+    // init
+
+    const page = await browser.newPage();
+
+    await page.goto(galleryUrl.toString());
+
+    console.log("Paused while anti-bot page is bypassed.");
+    const secondsToWait = parseInt(process.env.SECONDS_TO_WAIT || 10, 10);
+    console.log("Waiting", secondsToWait, "seconds.");
+    await page.waitForTimeout(secondsToWait * 1000);
+    console.log("Ok, unpaused and moving forward.");
+
+    console.log();
+
+    // title
+
+    const title = await page.evaluate(
+      () => document.querySelector("#info h1.title").textContent
+    );
+
+    console.log("Title:", title);
+
+    // pages
+
+    const pagesQttStr = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("#info .tag-container.field-name"))
+        .map((el) => el.textContent)
+        .map(
+          (textContent) =>
+            textContent.match(/Pages.*\n?.*(?<pagesQtt>\d+)/)?.groups.pagesQtt
+        )
+        .find((it) => it)
+    );
+    const pagesQtt = parseInt(pagesQttStr, 10);
+
+    console.log("Pages", pagesQtt);
+
+    // images
+
+    const someImageSrc = await page.evaluate(
+      () => document.querySelector("#thumbnail-container img").src
+    );
+    const someImageUrl = new URL(someImageSrc);
+
+    const imagesRootPathname = someImageUrl.pathname.replace(/\d+t.jpg/, "");
+    const imagesBase = `${galleryUrl.protocol}//i.${galleryUrl.hostname}/`;
+    const imageRootUrl = new URL(imagesRootPathname, imagesBase);
+
+    console.log("Images root URL:", imageRootUrl.toString());
+
+    const imagesUrls = Array(pagesQtt)
+      .fill("")
+      .map((_, index) => new URL(`${index + 1}.jpg`, imageRootUrl));
+
+    console.log("Printing images URLs...");
+    console.log(imagesUrls.map((it) => it.toString()).join("\n"));
+
+    // downloads
+    if (!storageRootPath) {
+      console.log("Bye!");
+      return;
+    }
+
+    console.log();
+    console.log("Preparing for download...");
+
+    if (fs.existsSync(storageRootPath)) {
+      console.log("Existing root folder found.");
+    } else {
+      console.log("Creating root folder...");
+      fs.mkdirSync(storageRootPath);
+      console.log("Done.");
+    }
+
+    const storageGaleryPath = path
+      .join(storageRootPath, `${galleryId} - ${title}`)
+      .replaceAll(" ", "_");
+    console.log("Gallery folder:", storageGaleryPath);
+    if (fs.existsSync(storageGaleryPath)) {
+      console.log("Existing gallery folder found.");
+    } else {
+      console.log("Creating gallery folder...");
+      fs.mkdirSync(storageGaleryPath);
+      console.log("Done.");
+    }
+
+    console.log();
+    console.log("Downloading now.");
+    const downloadPromiseMakers = imagesUrls.map(
+      (imageUrl, index) => () =>
+        new Promise((resolve) => {
+          process.stdout.write(imageUrl.toString() + " ... ");
+
+          const filename = `${index + 1}.jpg`;
+          const filePath = path.join(storageGaleryPath, filename);
+
+          if (fs.existsSync(filePath)) {
+            console.log("Already existed.");
+            return resolve();
+          }
+
+          https.get(imageUrl, (response) => {
+            const fileStream = fs.createWriteStream(filePath);
+            response.pipe(fileStream);
+            fileStream.on("finish", () => {
+              fileStream.close();
+              console.log("Created", filePath);
+              return resolve();
+            });
+          });
+        })
+    );
+
+    await downloadPromiseMakers.reduce(
+      (acc, makeDownloadPromise) => acc.then(makeDownloadPromise),
+      Promise.resolve()
+    );
+
+    console.log("Done.");
+  } catch (error) {
+    console.error("Error while scrapping:", error);
+  } finally {
+    await browser.close();
+  }
+})();
